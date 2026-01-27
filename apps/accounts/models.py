1cda173731772b9c80141bcff7b305b120b3ae5d
natan33 <srassis2014@gmail.com>
2026-01-27 17:25:29 -0300

    Initial Django project structure

---
+++ apps/accounts/models.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+
+    is_client = models.BooleanField(default=True)
+    is_barber = models.BooleanField(default=True)
+
+    phone = models.CharField(max_length=20, blank=True)
+
+
+class Client(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='client_profile'
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.get_full_name() or self.user.username
+     
+
+class Barber(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='barber_profile'
+    )
+
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.user.get_full_name() or self.user.username
