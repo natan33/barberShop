5f456c193f7bb96cc4053189b701202cbc2cb696
natan33 <srassis2014@gmail.com>
2026-01-27 17:49:39 -0300

    Create services app and Service model

--- apps/services/admin.py
+++ apps/services/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Service
 
-# Register your models here.
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "price",
+        "duration_minutes",
+        "is_active",
+        "created_at"
+    )
+
+    list_filter = ('is_active',)
+    search_fields = ('name',)
--- apps/services/models.py
+++ apps/services/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Service(models.Model):
+    name = models.CharField('Nome do serviço', max_length=100)
+    price = models.DecimalField(
+        'Preço',
+        max_digits=8,
+        decimal_places=2
+    )
+
+    duration_minutes = models.PositiveIntegerField(
+        "Duração (minutos)"
+    )
+
+    is_active = models.BooleanField(
+        "Ativo",
+        default=True
+    )
+
+    created_at = models.DateTimeField(
+        'Criado em',
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        'Atualizado em',
+        auto_now=True
+    )
+
+    class Meta:
+        verbose_name = "Serviço"
+        verbose_name_plural ="Serviços"
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name} - R$ {self.price}"
+    
+    
